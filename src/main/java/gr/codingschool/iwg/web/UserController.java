ee6dbe746fe3f4c5b3bfc6ac1e1626311b9ed40a
mhortis <mhortis@gmail.com>
2017-04-22 13:44:15 +0300

    Merge branch 'master' into games

--- src/main/java/gr/codingschool/iwg/web/UserController.java
+++ src/main/java/gr/codingschool/iwg/web/UserController.java
@@ -1,8 +1,11 @@
 package gr.codingschool.iwg.web;
 
+import gr.codingschool.iwg.model.Game;
 import gr.codingschool.iwg.model.User;
-import gr.codingschool.iwg.service.UserService;
+import gr.codingschool.iwg.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,12 +16,18 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class UserController {
     @Autowired
-    private UserService userService;
+    private GameService gameService;
 
     @RequestMapping(value = {"/user","/user/home"}, method = RequestMethod.GET)
-    public ModelAndView home(HttpSession session) {
+    public ModelAndView home(Pageable pageable, HttpSession session) {
         ModelAndView modelAndView = new ModelAndView();
         User loggedInUser = (User) session.getAttribute("user");
+
+        Page<Game> gamePage = gameService.findAllGames(pageable);
+        PageWrapper<Game> page = new PageWrapper<Game>(gamePage, "/user/home");
+
+        modelAndView.addObject("list", page.getContent());
+        modelAndView.addObject("page", page);
         modelAndView.addObject("user", loggedInUser);
         modelAndView.setViewName("user/index");
         return modelAndView;
