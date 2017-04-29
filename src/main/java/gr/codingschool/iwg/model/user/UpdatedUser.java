4e130502725c4ebe36328eeded3e3f8e0812d77b
cgdesinger <georgeantonopoulosga@gmail.com>
2017-04-29 19:24:29 +0300

    UpdateUser not functional & Profile interface almost complete

---
+++ src/main/java/gr/codingschool/iwg/model/user/UpdatedUser.java
@@ -0,0 +1,67 @@
+package gr.codingschool.iwg.model.user;
+
+/**
+ * Created by cgdesinger on 4/29/17.
+ */
+public class UpdatedUser {
+
+    private String username;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
